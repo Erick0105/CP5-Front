5d4443a5bcbc70a84d077101c0427b7ed254a018
fFukurou <supevi1@gmail.com>
2024-10-02 08:25:00 -0300

    added Footer, Header

--- worlds-collision/src/app/layout.tsx
+++ worlds-collision/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "Worlds in Collision",
@@ -13,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body>
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
