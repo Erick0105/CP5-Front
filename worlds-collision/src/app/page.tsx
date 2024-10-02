dd2baed18c5f80314d91b684d283e958311a51a0
fFukurou <supevi1@gmail.com>
2024-10-02 09:03:08 -0300

    Main megatron

--- worlds-collision/src/app/page.tsx
+++ worlds-collision/src/app/page.tsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <h1>As obras de <strong>Velikovsky </strong></h1>
-    </div>
+    <main>
+      <h1>As obras de --Velikovsky--</h1>
+      <h2>Essas obras de Valkovsky ajudaram a avançar a compreensão das interações cósmicas e suas consequências para a formação e evolução dos planetas, influenciando estudos posteriores na astrofísica e na cosmologia.</h2>
+      <ol>
+        <li><h3>Órbitas Planetárias e Instabilidade</h3>
+        <p>Nessa obra, Valkovsky estudou a dinâmica das órbitas planetárias, explorando como pequenas variações na gravitação poderiam levar a instabilidades.</p>
+        </li>
+      <li>
+      <h3>Cronologia dos Eventos</h3>
+      <p> Valkovsky se concentrou em estabelecer uma linha do tempo para eventos astronômicos importantes, ligando-os a fenômenos observáveis, como cometas e impactos.</p>
+      </li>
+      <li>
+      <h3>Forças Gravitacionais e Eletromagnéticas</h3>
+      <p>Valkovsky analisou a interação entre forças gravitacionais e eletromagnéticas, buscando entender como essas forças influenciam a dinâmica de corpos celestes.</p>
+      </li>
+      <li>
+      <h3>Velocidade e Trajetória de Vênus</h3>
+      <p>Valkovsky analisou a interação entre forças gravitacionais e eletromagnéticas, buscando entender como essas forças influenciam a dinâmica de corpos celestes</p>
+      </li>
+      </ol>
+
+
+    </main>
   )
 }
--- worlds-collision/src/components/Footer/Footer.tsx
+++ worlds-collision/src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
+      <h4><a href="https://www.nasa.gov/">NASA</a></h4>
         <h4>AutoSense &copy; - 2024 </h4>
+
         
     </footer>
   )
