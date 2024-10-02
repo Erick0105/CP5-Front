023fa51a0878aaeac4ca7a2b0e543741c16f282a
Erick0105 <erickaxs0105@gmail.com>
2024-10-02 10:04:01 -0300

    tentando fazer a merda da API

--- worlds-collision/src/components/Nav/Nav.tsx
+++ worlds-collision/src/components/Nav/Nav.tsx
@@ -8,7 +8,7 @@ export default function Nav() {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/orbitas">Órbitas e Instabilidade</Link></li>
             <li><Link href="/eventos">Cronologia de eventos</Link></li>
-            <li><Link href="/forças">Forças gravitacionais e eletromagnéticas</Link></li>
+            <li><Link href="/forcas">Forças gravitacionais e eletromagnéticas</Link></li>
             <li><Link href="/calculo">Cálculo</Link></li>
         </ul>
     </nav>
