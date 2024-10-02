bd2353d2ad070c7ab8f5849daca5adcaef3f8f62
LuizHNR <luizhneri12@gmail.com>
2024-10-02 08:36:12 -0300

    Not-fund

---
+++ worlds-collision/src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function NotFound() {
+    return (
+      <div>
+        <h1>404 - NotFound</h1>
+        <h2>This page could not be found.</h2>
+
+        <Image src="/img/3814348.jpg" alt="Página de erro." width={400} height={400}/>
+      </div>
+          
+    )
+  }
