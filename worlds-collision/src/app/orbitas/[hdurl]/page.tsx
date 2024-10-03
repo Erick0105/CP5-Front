bbbbbcf9f9601c776f9e1314eb4a4b9ff7356915
LuizHNR <luizhneri12@gmail.com>
2024-10-02 21:43:15 -0300

    nome parametro

---
+++ worlds-collision/src/app/orbitas/[hdurl]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Orbita({params}: {params:{hdurl:string}}) {
+    return (
+      <div>
+          <h2>Imagem {params.hdurl}</h2>
+      </div>
+    )
+  }
